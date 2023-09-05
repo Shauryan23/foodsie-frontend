3ba8ea91cd86bf542b9d32129afedcacdea36b05
Shauryan23 <shauryan23singh@gmail.com>
2023-09-06 01:46:50 +0530

    Login Page Completed

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,25 +1,90 @@
 import React from 'react';
 import './Login.css';
 import COVER_IMG from '../assets/images/cover1.jpg';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Input,
+  Checkbox,
+  Link,
+  Button,
+} from '@nextui-org/react';
 
 const Login = () => {
   return (
-    <div className="w-full h-screen flex items-start">
+    <div className="w-full h-screen flex">
       <div className="relative w-1/2 h-full flex flex-col">
-        <div className="absolute top-[3%] left-[8%]">
-          <h1 className="absolute text-6xl text-white font-bold my-4">
-            Foodsie.
+        <div className="absolute -top-[8%] left-[40%]">
+          <h1 className="absolute text-12xl text-[#F3EBE0] font-bold my-5">
+            Food
           </h1>
-          <h3 className="relative top-24 text-xl text-orange font-semibold">
-            Embrace your Cravings
-          </h3>
+        </div>
+        <div className="absolute -top-[8%] left-[103%]">
+          <h1 className="absolute text-12xl font-bold my-5">sie.</h1>
         </div>
 
         <img
           src={COVER_IMG}
           className="relative w-full h-full object-cover -z-20"
         />
-        <div className="gradient-overlay"></div>
+        <div className="gradient-overlay -z-10"></div>
+      </div>
+      <div className="flex flex-row min-w-1/2 justify-between items-center">
+        <Card shadow="lg" className="ml-[15%] -bottom-[8%]">
+          <CardHeader className="flex flex-col items-start">
+            <div className="px-5 pt-3">
+              <h2 className="text-xl font-bold pb-2">Login</h2>
+              <h4 className="text-lg font-normal">
+                Welcome back! Please enter your details.
+              </h4>
+            </div>
+          </CardHeader>
+          <CardBody className="pt-0">
+            <div className="w-full flex flex-col">
+              <div className="flex w-2/3 flex-wrap ml-4 mb-2 gap-4">
+                <Input type="email" variant="underlined" label="Email" />
+              </div>
+            </div>
+            <div className="w-full flex flex-col">
+              <div className="flex w-2/3 flex-wrap ml-4 mb-4 gap-4">
+                <Input type="text" variant="underlined" label="Password" />
+              </div>
+              <div className="flex flex-row justify-between">
+                <Checkbox defaultSelected={false} className="ml-2" size="sm">
+                  Remember me
+                </Checkbox>
+                <Link
+                  href="#"
+                  color="foreground"
+                  underline="hover"
+                  className="text-base font-normal"
+                >
+                  Forgot Password
+                </Link>
+              </div>
+              <div className="flex flex-row justify-center mt-5">
+                <Button
+                  color="warning"
+                  variant="solid"
+                  className="flex w-1/3 justify-center items-center"
+                >
+                  Login
+                </Button>
+              </div>
+              <div className="flex flex-row justify-center mt-5">
+                <Button
+                  color="default"
+                  variant="ghost"
+                  className="flex w-1/3 justify-center items-center"
+                >
+                  Register
+                </Button>
+              </div>
+            </div>
+          </CardBody>
+        </Card>
       </div>
     </div>
   );
